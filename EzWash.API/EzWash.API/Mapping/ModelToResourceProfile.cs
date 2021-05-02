8575a329be32b6b977d18e7b273dc0f846d6d439
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-05-02 12:08:53 -0500

    Created and implemented base files for Endpoint creation.

---
+++ EzWash.API/EzWash.API/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using EzWash.API.Domain.Models;
+using EzWash.API.Resources;
+
+namespace EzWash.API.Mapping
+{
+    public class ModelToResourceProfile: Profile
+    {
+        public ModelToResourceProfile()
+        {
+            //TODO: Create Model to Resource Mapping
+            
+            CreateMap<Department, DepartmentResource>();
+            
+        }
+    }
+}
