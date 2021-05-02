2aa0bebdeb652b1579e2d23d1ca96712177e6368
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-05-02 11:08:49 -0500

    Created Department repository interface.

---
+++ EzWash.API/EzWash.API/Domain/Persistence/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EzWash.API.Domain.Models;
+
+namespace EzWash.API.Domain.Persistence.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> ListAsync();
+        Task AddAsync(Department department);
+        Task<Department> FindById(int id);
+        void Update(Department department);
+        void Remove(Department department);
+    }
+}
