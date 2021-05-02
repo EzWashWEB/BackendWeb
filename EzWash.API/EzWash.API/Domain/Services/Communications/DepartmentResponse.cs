ce92a2734a1a7bf96db6a26b202e38f3c50e4194
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-05-02 12:09:11 -0500

    Department Endpoint done.

---
+++ EzWash.API/EzWash.API/Domain/Services/Communications/DepartmentResponse.cs
@@ -0,0 +1,15 @@
+﻿using EzWash.API.Domain.Models;
+
+namespace EzWash.API.Domain.Services.Communications
+{
+    public class DepartmentResponse: BaseResponse<Department>
+    {
+        public DepartmentResponse(Department resource) : base(resource)
+        {
+        }
+
+        public DepartmentResponse(string message) : base(message)
+        {
+        }
+    }
+}
